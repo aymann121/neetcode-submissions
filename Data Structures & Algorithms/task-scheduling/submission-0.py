980d758526eb551a5bbdb740df81cd4fc6483297
Ayman Noreldaim <84142410+aymann121@users.noreply.github.com>
2026-09-13 14:02:52 -0400

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        #availbleHeap - -numremaining, task
+        # cooldown - time when available, -numremaining, task
+        freq = Counter(tasks)
+        cooldownHeap = []
+        availableHeap = [(-val, key) for key, val in freq.items()]
+        heapq.heapify(availableHeap)
+        time = 0
+        
+        while cooldownHeap or availableHeap:
+               
+            while cooldownHeap and cooldownHeap[0][0] <= time:
+                e = heapq.heappop(cooldownHeap)
+                heapq.heappush(availableHeap, (e[1], e[2]))
+            
+            if availableHeap:
+                task = heapq.heappop(availableHeap)
+                if task[0] + 1 != 0:
+                  heapq.heappush(cooldownHeap, (time + n + 1, task[0] + 1, task[1]))
+            # print(cooldownHeap, availableHeap)
+            time +=1
+            
+        return time
+            
